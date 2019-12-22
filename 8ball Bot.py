9ec04ef81d6de711547d974222581611e8f3f053
Tesla16 <43827353+Tesla16@users.noreply.github.com>
2019-12-22 10:41:13 +0000

    Add files via upload

---
+++ 8ball Bot.py
@@ -0,0 +1,39 @@
+import discord
+import random
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = 'b.')
+
+@client.event
+async def on_ready():
+    print('8ball is ready')
+
+@client.command(aliases=['8ball'])
+async def _8ball(ctx, *,_8ball):
+    responses = ['As I see it, yes.',
+                 'Ask again later.',
+                 'Better not tell you now.',
+                 'Cannot predict now.',
+                 'Concentrate and ask again.',
+                 'Don’t count on it.',
+                 'It is certain.',
+                 'It is decidedly so.',
+                 'Most likely.',
+                 'My reply is no.',
+                 'My sources say no.',
+                 'Outlook not so good.',
+                 'Outlook good.',
+                 'Reply hazy, try again.',
+                 'Signs point to yes.',
+                 'Very doubtful.',
+                 'Without a doubt.',
+                 'Yes.',
+                 'Yes – definitely.',
+                 'You may rely on it.'
+                 ]
+    await ctx.send(f'Question: {_8ball}\nAnswer: {random.choice(responses)}')
+
+
+
+
+client.run("NjU4MjQ1MTczOTA1NTg4MjQz.Xf9Bsg.52goiS21fHoIzWNJ3iECswkZt6E")
